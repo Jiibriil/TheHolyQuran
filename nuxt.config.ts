ad60406d16378f733af2204343de5ae9cdba12cc
Jibril Mohamud <jibrilmohamod@gmail.com>
2023-12-11 20:26:37 +0300

    Add Tailwind CSS configuration and styles
    Update Nuxt configuration and dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["radix-vue"]
-})
+  css: ["~/assets/css/main.css"],
+  modules: [
+    "@nuxtjs/kinde",
+    "@vueuse/nuxt",
+    "radix-vue/nuxt",
+    "@nuxt/image",
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  // modules: ["radix-vue", "@vueuse/nuxt"],
+});
